3810601c160c7c0f83de83b32c347a84580dc896
vanaf25 <vanayfefilov777@gmail.com>
2024-08-19 21:18:43 +0200

    init

--- src/Cards/Cards.jsx
+++ src/Cards/Cards.jsx
@@ -1,18 +1,21 @@
 import React, {useEffect, useState} from 'react';
 import Card from "./Card/Card"
+import {Typography} from "@mui/material";
 const  BASE_URL="https://backend-two-xi-40.vercel.app/"
 const Cards = () => {
     const [cards,setCards]=useState([]);
+    const [isLoading,setIsLoading]=useState(false)
     useEffect(() => {
-        console.log(process.env);
+        setIsLoading(true)
         fetch(BASE_URL).then(res=>res.json()).then(res=>{
             console.log('res:',res);
             setCards(res);
-        });
+            setIsLoading(false)
+        }).catch(err=>console.log('err:Error'));
     }, []);
     return (
         <div>
-            {cards.map(card=><Card {...card}/>)}
+            {isLoading ? <Typography variant={"h4"} sx={{color:"#fff"}}>Loading...</Typography>: cards.map(card=><Card {...card}/>)}
         </div>
     );
 };
